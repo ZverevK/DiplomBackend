526dab2849eff21e5b0c8f116c3f3c4711952eae
ZverevK <zverev.a.konstantin@yandex.ru>
2020-12-22 16:22:40 +0300

    fix some errors

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 
-const { rateLimit } = require('./middlewares/rateLimit');
+const { limiter } = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const router = require('./routes/index');
 
 const app = express();
 const { PORT = 3000, DB_LINK = 'mongodb://localhost:27017/diplomdb' } = process.env;
@@ -17,7 +18,7 @@ mongoose.connect(DB_LINK, {
   useUnifiedTopology: true,
 });
 
-app.use(rateLimit);
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,7 +30,18 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+
+app.use('/', router);
+
 app.use(errorLogger);
 app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  return next();
+});
 
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -31,10 +31,4 @@ router.use('/', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
-router.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-  return next();
-});
+module.exports = router;
